be99ab90ee13b8a6d59196d71f6f2dfe8bad65a5
eddy9548 <eduardo@yahoo.com.br>
2015-10-29 20:44:37 -0200

    primeiro commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :topics
+  resources :categories
   mount Upmin::Engine => '/admin'
   root to: 'visitors#index'
   devise_for :users
---
+++ spec/factories/posts.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :post do
+    content "MyString"
+text "MyString"
+topic nil
+user nil
+  end
+
+end
